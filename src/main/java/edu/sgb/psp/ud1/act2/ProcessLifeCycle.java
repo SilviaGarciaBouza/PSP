229d0913169d781628d2e6870757d74b292f8ece
Silvia García Bouza <126358009+SilviaGarciaBouza@users.noreply.github.com>
2025-09-22 10:57:48 +0200

    Update ProcessLifeCycle.java

--- src/main/java/edu/sgb/psp/ud1/act2/ProcessLifeCycle.java
+++ src/main/java/edu/sgb/psp/ud1/act2/ProcessLifeCycle.java
@@ -9,73 +9,70 @@ import java.util.concurrent.TimeUnit;
 import static java.time.Instant.now;
 
 public class ProcessLifeCycle {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        //1.Haz que el valor de la IP sea leída por teclado (clase Scanner)
         Scanner sc= new Scanner(System.in);
-        try {
-            //IMPORTANTE: Primero empezar uno y terminarlo y despues empezar y terminar el segundo
-            // NEW: construimos el proceso
-            System.out.println("Estado: NUEVO (NEW)");
-            System.out.println("Escribe la ip: ");
-            String ip= sc.nextLine();
-
-            ProcessBuilder pb = new ProcessBuilder("ping",ip, "-n","100000");
-            ProcessBuilder pb2 = new ProcessBuilder("ping",ip, "-n", "1");
-
-           // pb.inheritIO();
-            pb2.inheritIO();
-            pb.inheritIO();
-
-
-            /*si se haqce el inerit es que lo escribe en el log y sino en elarchivo asi q o se hace uno oo el otro
-            pb.redirectError(new File("ping_error_output.txt"));
-            pb.redirectOutput(new File("ping_std_output.txt"));
-            System.out.println("Estado: LISTO (READY)");
-            pb2.redirectError(new File("ping_error_output.txt"));
-            pb2.redirectOutput(new File("ping_std_output.txt"));
-            System.out.println("Estado: LISTO el Proceso2 (READY)");
-*/
-
-            Instant start= Instant.now();
-            Process p = pb.start();
-            boolean exitCode= p.waitFor(5, TimeUnit.SECONDS);
-
-            if(!exitCode)
-            {
-                System.out.println("El proceso ha superado el tiempo, procedera a destruirse");
-                p.destroyForcibly();
-
-            }
-            Instant end= Instant.now();
-
-            Instant start2= Instant.now();
-            Process p2 = pb2.start();
-
-
-
-            boolean exitCode2= p2.waitFor(5, TimeUnit.SECONDS);
-
-            if(!exitCode2)
-            {
-                System.out.println("El proceso ha superado el tiempo, procedera a destruirse");
-                p2.destroyForcibly();
-
-            }
-            Instant end2= Instant.now();
-
-          Duration diff= Duration.between(end,start);
-          Duration diff2= Duration.between(end2,start2);
-
-            System.out.println("Estado: EN EJECUCIÓN (RUNNING)");
-           // System.out.println(p.info().totalCpuDuration());
-            System.out.println("La duracion del primer proceso es: "+diff+(exitCode?"El proceso terminó solo":"El proceso se destruyó al alcanzar el tiempo max"));
-
-          //  System.out.println(p2.info().totalCpuDuration());
-            System.out.println("La duracion del segundo proceso es: "+diff2+(exitCode2?"El proceso terminó solo":"El proceso se destruyó al alcanzar el tiempo max"));
-
-
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Introduce la ip");
+        String ip=sc.nextLine();
+       try{
+           ProcessBuilder pb= new ProcessBuilder("ping", ip, "-n","1");
+           //2. Investiga los métodos disponibles en la API de ProcessBuilder
+           //y haz que se vuelque la salida estándar a un fichero llamado
+           // ping_std_output.txt y la salida de error a ping_error_output.txt
+           //se puede usar // pb.inheritIO(); y lo envia por pantalla en lugar de lo sig
+           pb.redirectOutput(new File("ping_std_output.txt"));
+           pb.redirectError(new File("ping_error_output.txt"));
+           System.out.println("ESTADO LISTO (READY)");
+           //3.Medir cuánto tarda el proceso externo y, si se pasa de un
+           // tiempo máximo (timeout), terminarlo forzosamente.
+           // Investiga la clase Process para ver como esperar un timeout
+           // y como forzar un cierre del proceso.
+           Instant start= Instant.now();
+           Process process= pb.start();
+           System.out.println("ESTADO EN EJECUCIÓN (RUNNING)");
+           boolean exitCode= process.waitFor(5, TimeUnit.SECONDS);
+           if (!exitCode){
+               process.destroyForcibly();
+               System.out.println("ESTADO TERMINADO (TERMINATED) FORZADAMENTE");
+           }
+           Instant end= Instant.now();
+           Duration diff= Duration.between(start, end);
+           System.out.println("Tiempo de ejecución: " + diff.toMillis());
+           //4.Lanza dos procesos de ping independientes
+           // y comprueba cuando acaba cada un.
+           ProcessBuilder pb2= new ProcessBuilder("ping", ip, "-n","10000");
+           ProcessBuilder pb3= new ProcessBuilder("ping", ip, "-n","1");
+           pb2.redirectOutput(new File("ping_std_output.txt"));
+           pb2.redirectError(new File("ping_error_output.txt"));
+           pb3.redirectOutput(new File("ping_std_output.txt"));
+           pb3.redirectError(new File("ping_error_output.txt"));
+           System.out.println("Proceso 2: ESTADO LISTO (READY)");
+           System.out.println("Proceso 3: ESTADO LISTO (READY)");
+           Instant start2= Instant.now();
+           Process process2= pb.start();
+           System.out.println("Proceso 2:ESTADO EN EJECUCIÓN (RUNNING)");
+           boolean exitCode2= process.waitFor(5, TimeUnit.SECONDS);
+           if (!exitCode2){
+               process2.destroyForcibly();
+               System.out.println("Proceso 2: ESTADO TERMINADO (TERMINATED) FORZADAMENTE");
+           }
+           Instant end2= Instant.now();
+           Duration diff2= Duration.between(start2, end2);
+           System.out.println("Tiempo de ejecución del proceso 2: " + diff.toMillis());
+           Instant start3= Instant.now();
+           Process process3= pb3.start();
+           System.out.println("Proceso 3: ESTADO EN EJECUCIÓN (RUNNING)");
+           boolean exitCode3= process.waitFor(5, TimeUnit.SECONDS);
+           if (!exitCode3){
+               process3.destroyForcibly();
+               System.out.println("Proceso 3: ESTADO TERMINADO (TERMINATED) FORZADAMENTE");
+           }
+           Instant end3= Instant.now();
+           Duration diff3= Duration.between(start3, end3);
+           System.out.println("Proceso 3: Tiempo de ejecución: " + diff.toMillis());
+       }
+       catch (Exception e) {
+           System.out.println(e.getMessage());
+       }
     }
 }
