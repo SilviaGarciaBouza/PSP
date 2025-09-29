1c2143f415d353c0b6d8b3a630b9a304dc2e07a0
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-09-29 10:00:17 +0200

    added new exercise act6

--- src/main/java/edu/sgb/psp/ud1/act6/act6.java
+++ src/main/java/edu/sgb/psp/ud1/act6/act6.java
@@ -3,27 +3,34 @@ package edu.sgb.psp.ud1.act6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+* Realiza un programa Java que solicite a un usuario el nombre de un proceso que quiere buscar.
+* El usuario introducirá una palabra contenida en el proceso, por ejemplo para buscar todos
+* los procesos de chrome podría escribir “chro”.
+
+El programa mostrará por pantalla todos los procesos que contengan la palabra introducida por el usuario.
+* */
 public class act6 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //pido el nombre del proceso que quiero buscar
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Escribe el nombre del proceso que quieres buscar: ");
+        String proceso= sc.nextLine();
+
         try {
-            System.out.println("Escribe el nombre del proceso: ");
-            String comand = sc.nextLine();
-            // TODO: Crea el proceso para ejecutar "tasklist" y lánzalo
-            String filter = "IMAGENAME eq " + comand + "*";
-            ProcessBuilder pb = new ProcessBuilder("tasklist", "/fi", filter );
-           // pb.inheritIO();
-            Instant start= Instant.now();
-            Process process= pb.start();
+            //creo el proceso
+          ProcessBuilder pb= new ProcessBuilder("tasklist");
+          Process process= pb.start();
             // Captura y guarda la salida del comando "tasklist" en una List
             List<String> outputLines = getProcessOutputLines(process);
-            outputLines.forEach(e-> System.out.println(e));
 
-            // TODO: Muestra solo que se desea mostrar (aplicando el filtro)
+            //  Muestra solo que se desea mostrar (aplicando el filtro)
+            outputLines.stream().filter(e-> e.contains(proceso)).forEach(e-> System.out.println(e));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,13 +45,17 @@ public class act6 {
      * @throws IOException
      */
     private static List<String> getProcessOutputLines(Process process) throws IOException {
-        try(
-                BufferedReader br= new BufferedReader(process.inputReader())
-                ){return br.lines().toList();
-
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            return List.of();
+        List<String> outputStrings= new ArrayList<>();
+        try(    InputStreamReader isr= new InputStreamReader(process.getInputStream());
+                BufferedReader br= new BufferedReader(isr)
+                ) {
+        String line;
+        while((line=br.readLine())!=null){
+            outputStrings.add(line);
         }
+        }
+        return outputStrings;
+
     }
-}
+    }
+
